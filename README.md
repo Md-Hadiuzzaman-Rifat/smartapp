b2d18044e9d0ee3d106b631b53e4d11059159e99
Md Hadiuzzaman Rifat <hadirifat100@gmail.com>
2023-03-07 15:34:46 +0600

    complete header file without motion

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
-What have I learn from the project:
-1. firstly I have to install Vite not react.
-2. I cant use bam rules in where I use scss.
-3. bam nested not working like,
+<div>
+    ${logo-create}What have I learn from the project:
+    1. firstly I have to install Vite not react.
+    2. I cant use bam rules in where I use scss.
+    3. bam nested not working like,
 
-body{
-    font:{
-        size:24px;
+    body{
+        font:{
+            size:24px;
+        }
+    }     //*****will work****** where as
+
+    .header{
+        logo{
+
+        }
     }
-}     //*****will work****** where as
+    4. I will use module.scss
+    5. css module doesn`t support - only support _ . {`${logo-create}`}. It would be {${logo_create}}
+    6. I have nothing to worry about importing scss file. I will directly import .scss file in Index.js file
+    7. I will not use _file.scss as it create problem importing in index.js file.
+    8. React Icon use bootstrap icon and many other icons . so first you have to look at the whole page.
+    <li> <p><BiPhone size={"40px"}/></p>  </li>
+    9.       svg {
+        color: $secondary;
+        background-color: white;
+        padding: 10px;
+        border-radius: 999px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
+      }
 
-.header{
-    logo{
-
-    }
-}
-4. I will use module.scss
-5. css module doesn`t support - only support _ . {`${logo-create}`}. It would be {${logo_create}}
+</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React from "react";
+import { BiPhone } from "react-icons/bi";
 import classes from "./Header.module.scss";
 
 const Header = () => {
-    return (
-        <div className={`${classes.header}`}>
-            <div className={`${classes.header_logo}`}>
-                <h1>Smart App</h1>
-            </div>
-            <div className="header_navigation">
-                <nav>
-                    <ul>
-                        <li><a href="#">service</a></li>
-                        <li><a href="#">experience</a></li>
-                        <li><a href="#">portfolio</a></li>
-                        <li><a href="#">testimonials</a></li>
-                        <li><a href="#">+880 1910803372</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${classes.header} flexBetween innerWidth paddings`}>
+      <div className={`${classes.header_logo}`}>
+        <h1>Smart App</h1>
+      </div>
+      <div className={`${classes.header_navigation}`}>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">service</a>
+            </li>
+            <li>
+              <a href="#">experience</a>
+            </li>
+            <li>
+              <a href="#">portfolio</a>
+            </li>
+            <li>
+              <a href="#">testimonials</a>
+            </li>
+            <li>
+              <a href="#">
+                {" "}
+                <div className="flexCenter">
+                  <p style={{marginRight:"10px"}}>+880 1910803372</p>
+                  <BiPhone size={"40px"} />
+                </div>{" "}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
