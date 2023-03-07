b8642c3456ada2c955c628ec719dc24460f77eb7
Md Hadiuzzaman Rifat <hadirifat100@gmail.com>
2023-03-08 02:25:15 +0600

    complete header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,24 @@
+import { motion } from "framer-motion";
 import React from "react";
 import { BiPhone } from "react-icons/bi";
+import { headerVariants } from "../../utils/motion";
 import classes from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <div className={`${classes.header} flexBetween innerWidth paddings`}>
+    <motion.div
+      variants={headerVariants}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${classes.header} flexBetween innerWidth paddings`}
+    >
       <div className={`${classes.header_logo}`}>
         <h1>Smart App</h1>
       </div>
       <div className={`${classes.header_navigation}`}>
         <nav>
-          <ul>
+          <ul className={classes.menu}>
             <li>
               <a href="#">service</a>
             </li>
@@ -27,7 +35,7 @@ const Header = () => {
               <a href="#">
                 {" "}
                 <div className="flexCenter">
-                  <p style={{marginRight:"10px"}}>+880 1910803372</p>
+                  <p style={{ marginRight: "10px" }}>+880-1910803372</p>
                   <BiPhone size={"40px"} />
                 </div>{" "}
               </a>
@@ -35,7 +43,7 @@ const Header = () => {
           </ul>
         </nav>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
