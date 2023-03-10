e5e81fa2a3a45675744b5bf0818d86beaff05b87
Md Hadiuzzaman Rifat <hadirifat100@gmail.com>
2023-03-11 03:38:16 +0600

    i am tired. in an on the way of worker page structure

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 const Work = () => {
     return (
         <div>
-            <div className='innerWidth paddings yPaddings'>
-                
+            <div className='innerWidth paddings '>
+                <h1 style={{textAlign:"center"}} className='primaryText yPaddings'>My Work Experience</h1>
+                <div className="worker_content flexBetWeen">
+                    <div className="worker_content_duration"></div>
+                    <div className="worker_content_designation"></div>
+                </div>
             </div>
         </div>
     );
