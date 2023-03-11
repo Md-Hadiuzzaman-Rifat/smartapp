e29a67fa684b5a53b02ff3a4e79f14921187495c
Md Hadiuzzaman Rifat <hadirifat100@gmail.com>
2023-03-11 13:35:25 +0600

    worker section done without motion

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { workExp } from "../../utils/data.js";
+import classes from './Work.module.scss';
 
 const Work = () => {
     return (
         <div>
-            <div className='innerWidth paddings '>
-                <h1 style={{textAlign:"center"}} className='primaryText yPaddings'>My Work Experience</h1>
-                <div className="worker_content flexBetWeen">
-                    <div className="worker_content_duration"></div>
-                    <div className="worker_content_designation"></div>
+            <div className='innerWidth paddings'>
+                <div className={classes.worker_details}>
+                    {
+                        workExp.map((person,id)=>{
+                            return(
+                                <div key={id} className={classes.worker_details_content}>
+                                    <div className={classes.worker_details_content_left}>
+                                        <h3>{person.place}</h3>
+                                        <span>{person.tenure}</span>
+                                    </div>
+                                    <div className={classes.worker_details_content_right}>
+                                        <h3>{person.role}</h3>
+                                        <span>{person.detail}</span>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
